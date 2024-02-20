a0b9fdcf9a899dd8833ca966472588d978872eb2
Samq64 <81489795+Samq64@users.noreply.github.com>
2024-02-20 14:43:36 -0500

    Finish accidental Hypridle/lock commit

--- .local/bin/idle.sh
+++
@@ -1,6 +0,0 @@
-# Based on the swayidle man page example
-swayidle -w \
-	timeout 300 'gtklock -i' \
-	timeout 600 'swaymsg "output * dpms off"' \
-		resume 'swaymsg "output * dpms on"' \
-	before-sleep 'gtklock -i'
