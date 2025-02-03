13a1c52ab247c4c09911618f1e075cce00f72055
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-02-02 19:28:49 -0500

    Volume script Mako and POSIX compatibility

--- .local/bin/volume.sh
+++ .local/bin/volume.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 # Based on https://wiki.archlinux.org/title/Dunst#Using_dunstify_as_volume/brightness_level_indicator
 
-# Arbitrary but unique message tag
-msgTag="myvolume"
+msgTag="volume"
 
-if [[ $1 == toggle ]]; then
+if [ "$1" = "toggle" ]; then
     wpctl set-mute @DEFAULT_SINK@ toggle
 else
-    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ $@
+    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ "$@"
 fi
 
 volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print$2*100}')
 mute=$(wpctl get-volume @DEFAULT_SINK@ | awk '{print$3}')
-if [[ $volume == 0 || "$mute" == "[MUTED]" ]]; then
+if [ "$volume" -eq 0 ] || [ "$mute" = "[MUTED]" ]; then
     # Show the sound muted notification
-    dunstify -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
+    notify-send -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
 else
     # Show the volume notification
-    dunstify -a "changeVolume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
+    notify-send -a "changeVolume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
     -h int:value:"$volume" "Volume: ${volume}%"
 fi
 
