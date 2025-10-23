a4476554158e7ca511f5f9996449e3972824e66f
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-10-22 21:27:43 -0400

    Add microphone to volume script

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
-
-# Based on https://wiki.archlinux.org/title/Dunst#Using_dunstify_as_volume/brightness_level_indicator
+# Adapted from https://wiki.archlinux.org/title/Dunst#Using_dunstify_as_volume/brightness_level_indicator
+# Usage: power.sh {toggle|X%+|X%-} [mic]
 
 msgTag="volume"
-
-if [ "$1" = "toggle" ]; then
-    wpctl set-mute @DEFAULT_SINK@ toggle
-    wpctl set-mute @DEFAULT_SOURCE@ toggle
+if [ "$2" = "mic" ]; then
+    prefix=Microphone
+    device=@DEFAULT_SOURCE@
 else
-    wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ "$@"
+    prefix=Volume
+    device=@DEFAULT_SINK@
 fi
 
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print$2*100}')
-mute=$(wpctl get-volume @DEFAULT_SINK@ | awk '{print$3}')
+if [ "$1" = "toggle" ]; then
+    wpctl set-mute $device toggle
+else
+    wpctl set-volume -l 1.5 $device "$1"
+fi
+
+volume=$(wpctl get-volume $device | awk '{print$2*100}')
+mute=$(wpctl get-volume $device | awk '{print$3}')
 if [ "$volume" -eq 0 ] || [ "$mute" = "[MUTED]" ]; then
     # Show the sound muted notification
-    notify-send -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted"
+    notify-send -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "${prefix} muted"
 else
     # Show the volume notification
     notify-send -a "changeVolume" -u low -i audio-volume-high -h string:x-dunst-stack-tag:$msgTag \
-    -h int:value:"$volume" "Volume: ${volume}%"
+    -h int:value:"$volume" "${prefix}: ${volume}%"
 fi
 
-# Play the volume changed sound
-canberra-gtk-play -i audio-volume-change -d "changeVolume"
+[ -z "$2" ] && canberra-gtk-play -i audio-volume-change -d "changeVolume"
