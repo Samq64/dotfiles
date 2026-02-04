ab44f99fda43e415f9fba7e3add4eadf927f8d78
Samq64 <81489795+Samq64@users.noreply.github.com>
2026-02-04 15:42:47 -0500

    Tweak shell scripts

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias v="nvim"
 alias l="ls -Alh --color=always --group-directories-first"
 alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+alias lazydots='lazygit --git-dir $HOME/.dotfiles --work-tree $HOME'
 
 # Change working directory with Yazi
 function y() {
