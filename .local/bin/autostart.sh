fb1ff8dad4948fe33d33c45be1f0160f7e090b29
Samq64 <81489795+Samq64@users.noreply.github.com>
2026-03-09 13:17:49 -0400

    Mango only dotfiles

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-wm=$XDG_CURRENT_DESKTOP
-[ "$wm" = "Hyprland" ] && wm=hypr
-
 swaybg -m fill -i ~/.local/share/wallpaper &
-waybar -c ~/.config/"$wm"/waybar.jsonc &
+waybar &
 wl-clip-persist -c regular &
 mako &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 path=~/img/screenshots/$(date +%F_%H-%M-%S).png
-
-case $XDG_CURRENT_DESKTOP in
-    "Hyprland")
-        # https://www.reddit.com/r/hyprland/comments/1cbetfa/comment/mbg9o4y/
-        monitor="$(hyprctl monitors | awk '/Monitor/{mon=$2} /focused: yes/{print mon}')";;
-    "mango")
-        monitor="$(mmsg -g -o | awk '{ if($3 == 1 && $2 == "selmon") print $1}')";;
-esac
-
+monitor="$(mmsg -g -o | awk '{ if ($3 == 1 && $2 == "selmon") print $1}')"
 if [ "$1" = "satty" ]; then
     grim -c -o "$monitor" -t ppm - | satty --output-filename $path --filename -
 else
