a90f5da612534188a7ec3e98d79f7cdfbebd4c70
Samq64 <81489795+Samq64@users.noreply.github.com>
2026-03-09 13:17:49 -0400

    Mango only dotfiles

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 path=~/img/screenshots/$(date +%F_%H-%M-%S).png
-
-case $XDG_CURRENT_DESKTOP in
-    "Hyprland")
-        # https://www.reddit.com/r/hyprland/comments/1cbetfa/comment/mbg9o4y/
-        monitor="$(hyprctl monitors | awk '/Monitor/{mon=$2} /focused: yes/{print mon}')";;
-    "mango")
-        monitor="$(mmsg -g -o | awk '$2 == "selmon" && $3 == 1 {print $1}')"
-esac
+monitor="$(mmsg -g -o | awk '{ if ($3 == 1 && $2 == "selmon") print $1}')"
 
 if [ "$1" = area ]; then
     still -p -c 'grim -g "$(slurp -d)" - | wl-copy'
