007e0837a8d0a6d1bf83e024c7f901e4a5cc8e6f
Samq64 <81489795+Samq64@users.noreply.github.com>
2026-03-09 12:51:09 -0400

    Fix floaterm.sh, mddle-click waybar to mute mic

--- .local/bin/floaterm.sh
+++ .local/bin/floaterm.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 foot_opts=('--app-id=floaterm')
-[[ "$1" == "--hold" ]] && foot_opts+=('--hold'); shift
+if [[ "$1" == "--hold" ]]; then
+    foot_opts+=('--hold')
+    shift
+fi
 cmd="sleep 0.1; $*" # Add delay for window rule to fully apply
 exec foot "${foot_opts[@]}" sh -c "$cmd"
